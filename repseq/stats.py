be756c80509a805dbd1d01b1ee970d0d0386a5e5
mmjmike <mikhail.myshkin@phystech.edu>
2023-11-17 15:13:26 +0300

    added docs and kwargs to generic calculation

--- repseq/stats.py
+++ repseq/stats.py
@@ -79,13 +79,14 @@ def calc_segment_usage(clonosets_df, segment="v", cl_filter=None, table="long"):
     if table not in table_options:
         raise ValueError(f"Unknown value for 'table' parameter. Possible options: {', '.join(table_options)}")
 
-    segment_to_function_dict = {"v": calc_v_usage_cl,
-                                "j": calc_j_usage_cl,
-                                "c": calc_c_usage_cl}
+    # segment_to_function_dict = {"v": calc_v_usage_cl,
+    #                             "j": calc_j_usage_cl,
+    #                             "c": calc_c_usage_cl}
+    possible_segments = ["v", "j", "c"]
     segment = segment.lower()
-    if segment not in segment_to_function_dict.keys():
-        raise ValueError(f"Wrong segment value. Possible values: {', '.join(segment_to_function_dict.keys())}")
-    df = generic_calculation(clonosets_df, segment_to_function_dict[segment], clonoset_filter=cl_filter, program_name="CalcSegmentUsage")
+    if segment not in possible_segments:
+        raise ValueError(f"Wrong segment value. Possible values: {', '.join(possible_segments)}")
+    df = generic_calculation(clonosets_df, calc_segment_usage_cl, clonoset_filter=cl_filter, program_name="CalcSegmentUsage", segment=segment)
     df = df.fillna(0)
     if table == "wide":
         return df
@@ -295,7 +296,41 @@ def calculate_diversity_stats_cl(clonoset_in, colnames=None):
     return result
 
 
-def generic_calculation(clonosets_df_in, calc_function, clonoset_filter=None, program_name="Calculation", iterations=1, seed=None, drop_small_samples=False):
+def generic_calculation(clonosets_df_in, calc_function, clonoset_filter=None, program_name="Calculation", iterations=1, seed=None, drop_small_samples=False, **kwargs):
+    '''
+    Main function that applies batch calculations for multiple clonosets
+    using a `calc_function`. It checks inputs, checks if clonotype counts are 
+    coherent with downsample or top numbers in clonoset filter.
+    All filters and calc_function are applied to each clonoset in parallel, if
+    several cores are available.
+    After all calculations are finished, this function combines the results into 
+    one pd.DataFrame.
+
+    Args:
+        clonosets_df (pd.DataFrame): dataframe, containing two required columns: 
+            `sample_id` and `filename`. Also recommended to have `chain` column in this DF.
+            `sample_id`'s or `sample_id`+`chain` combinations must be unique in this DF.
+        calc_function (function_name): function, applicable to a single clonoset (pd.DataFrame)
+            in VDJtools-like format that returns a dictionary of properties+values as output.
+        cl_filter (Filter, optional): clonoset filter - object from `clone_filter.py` module.
+        program_name (str): the name of applied calculation, it is shown in the progress-bar
+        iterations (int): number of iterations to obtain mean values for calculations when
+            random processes (downsampling/mix_tails) in clonoset filter are applied.
+            Recommended to use 3-5 iterations.
+        seed (hashable): seed for random events (downsampling/mix_tails). It overrides the 
+            values, specified in `cl_filter`.
+        drop_small_samples (bool): `True` - samples, that can't be downsampled or top-cropped
+            because of lack of counts/clonotypes will be dropped before the calculation.
+            `False` - small samples will be taken into account, but with fewer counts/clonotypes 
+            than those with enough counts/clonotypes.
+
+    Returns:
+        df (pd.DataFrame): resulting DataFrame, with `sample_id` and `chain` columns and properties
+            columns for each clonoset.
+
+    '''
+    
+    
     columns_retain = ["sample_id"]
     clonosets_df = clonosets_df_in.copy()
     
@@ -314,7 +349,7 @@ def generic_calculation(clonosets_df_in, calc_function, clonoset_filter=None, pr
             raise ValueError("Clonoset_df contains nonunique sample_id+chain combinations")
         if len(clonosets_df) != len(clonosets_df.sample_id.unique()):
             clonosets_df["sample_id"] = clonosets_df["sample_id"] + "_" + clonosets_df["chain"]
-            split_chain_after_calculation = True
+            split_chain_after_calculation = False
 
     random_filter = False
     need_downsample = False
@@ -403,9 +438,9 @@ def generic_calculation(clonosets_df_in, calc_function, clonoset_filter=None, pr
         if sample_id in exclude_samples:
             continue
         if clonoset_filter is not None:
-            task = (sample_id, filename, calc_function, clonoset_filter.spawn(), iterations, seed, program_name, random_filter)
+            task = (sample_id, filename, calc_function, clonoset_filter.spawn(), iterations, seed, program_name, random_filter, kwargs)
         else:
-            task = (sample_id, filename, calc_function, None, iterations, seed, program_name, random_filter)
+            task = (sample_id, filename, calc_function, None, iterations, seed, program_name, random_filter, kwargs)
         tasks.append(task)
     
     results = run_parallel_calculation(perform_generic_calculation_mp, tasks, program_name, object_name="calcultaion(s)")
@@ -416,7 +451,22 @@ def generic_calculation(clonosets_df_in, calc_function, clonoset_filter=None, pr
     return df
     
 def perform_generic_calculation_mp(args):
-    (sample_id, filename, calc_function, clonoset_filter, iterations, seed, program_name, random_filter) = args
+    '''
+    A single-core "worker" for `generic_calculation` function.
+    It applies clonoset filter (several times if `iterations` > 1) and performes a
+    calculation for a single filtered clonoset.
+
+
+    Args:
+        args (tuple): tuple, containing all required parameters
+    
+    Returns:
+        clonoset_result (dict): a dictionary of parameters, calculated for given clonoset
+            and averaged for iterations (if > 1)
+    '''
+    
+    
+    (sample_id, filename, calc_function, clonoset_filter, iterations, seed, program_name, random_filter, kwargs) = args
     clonoset = read_clonoset(filename)
     colnames = get_column_names_from_clonoset(clonoset)
         
@@ -435,7 +485,7 @@ def perform_generic_calculation_mp(args):
         filtered_clonosets.append(filtered_clonoset)
     
     for filtered_clonoset in filtered_clonosets:
-        clonoset_results.append(calc_function(filtered_clonoset))
+        clonoset_results.append(calc_function(filtered_clonoset, **kwargs))
     clonoset_result.update(pd.DataFrame(clonoset_results).mean().to_dict())
     
     return clonoset_result
