b6ae8ecc0fb5213fe3b4285f5f611603214f25ee
Mikhail Myshkin <mikhailmyshkin.ibch@gmail.com>
2025-05-05 11:28:20 +0300

    diff exp module: one sided mann whitney for group_vs_all and logFC > 0 filter

--- repseq/diffexp.py
+++ repseq/diffexp.py
@@ -34,12 +34,7 @@ def wilcox_diff_expression(count_table, sample_metadata, min_samples=2, count_th
         
         df = calc_mann_whitney_for_group_pair(count_table, pair, group1_samples, group2_samples, count_threshold, min_samples)
         
-        result_df = df
-        result_df = result_df.sort_values(by="p_adj")
-
-        # cut the table by pval_cutoff
-        result_df = result_df[result_df.p_adj <= pval_cutoff] 
-        return result_df.reset_index(drop=True)
+        result_df_main = df.sort_values(by="p_adj")
     
     # more than two groups
     else:
@@ -77,19 +72,13 @@ def wilcox_diff_expression(count_table, sample_metadata, min_samples=2, count_th
         result_df = pd.concat(result_dfs).reset_index(drop=True).merge(count_table)
         
         result_df_main = result_df[result_df.pair.str.contains("_vs_all")].copy()
-        
-        # implement only one-sided test, as the group which in vs_all is supposed to be
-        # overexpressed
-        result_df_main = result_df_main.query("logFC > 0")
 
         # perform FDR muliple test correction
         result_df_main["p_adj"] = multipletests(result_df_main["p"], alpha=0.05, method='fdr_bh', is_sorted=False, returnsorted=False)[1]
 
-        # cut the table by pval_cutoff
-        if pval_cutoff is not None:
-            if pval_cutoff <= 0 or pval_cutoff > 1:
-                raise ValueError("pval_cutoff must be in range (0,1]")
-            result_df_main = result_df_main[result_df_main["p_adj"] <= pval_cutoff]
+        # In the Group vs. All setting we are interested in only those clonotypes that are greater
+        # in the group rather than in All other groups
+        result_df_main = result_df_main.query("logFC > 0")
         
         if len(result_df_main) > 0:
             pairs_compare_results = []
@@ -108,9 +97,14 @@ def wilcox_diff_expression(count_table, sample_metadata, min_samples=2, count_th
                 
             pairs_compare_results_df = pd.DataFrame(pairs_compare_results, columns=pairs_compare_columns).set_index("i")
             result_df_main = result_df_main.merge(pairs_compare_results_df, left_index=True, right_index=True)
-            
-        return result_df_main
+    
+    # cut the table by pval_cutoff
+    if pval_cutoff is not None:
+        if pval_cutoff <= 0 or pval_cutoff > 1:
+            raise ValueError("pval_cutoff must be in range (0,1]")
+        result_df_main = result_df_main[result_df_main["p_adj"] <= pval_cutoff]
 
+    return result_df_main
 
 
 def find_p_val_and_logFC_for_pair(feature_id, group1, group2, full_result_df):
@@ -135,18 +129,27 @@ def calc_mann_whitney_for_group_pair_mp(args):
     return calc_mann_whitney_for_group_pair(*args)
 
 def calc_mann_whitney_for_group_pair(count_table, pair, group1_samples, group2_samples, count_threshold, min_samples):
+    # Alternative hypothesis for mann-whitney test:
+    # if it is between group comparison, then perform two-sided test
+    alternative = "two-sided"
+    # if it is SomeGroup vs. All comparison we are expecting that the group has 'greater' clone count
+    if "_vs_all" in pair:
+        alternative = "greater"
+
     df = count_table[group1_samples + group2_samples].copy()
     df = df[df.select_dtypes(include=['float', 'int']).apply(lambda x: x > count_threshold).sum(axis=1) >= min_samples]
-    df[["logFC", "U", "p"]] = df.apply(lambda x: mann_whitney_two_groups(x, group1_samples, group2_samples), axis=1, result_type='expand')
+    df[["logFC", "U", "p"]] = df.apply(lambda x: mann_whitney_two_groups(x, group1_samples, group2_samples,
+                                                                         alternative=alternative), axis=1,
+                                                                                                 result_type='expand')
     df["pair"] = pair
     df["p_adj"] = multipletests(df["p"], alpha=0.05, method='fdr_bh', is_sorted=False, returnsorted=False)[1]
     df = df.merge(count_table[["feature_id"]], left_index=True, right_index=True)[["feature_id", "logFC", "U", "p", "p_adj", "pair"]]
     return df
 
-def mann_whitney_two_groups(row, group1_samples, group2_samples):
+def mann_whitney_two_groups(row, group1_samples, group2_samples, alternative="two-sided"):
     values1 = row[group1_samples]
     values2 = row[group2_samples]
-    U,p = scipy.stats.mannwhitneyu(values1, values2)
+    U,p = scipy.stats.mannwhitneyu(values1, values2, alternative=alternative)
     mean1 = np.mean(values1)
     mean2 = np.mean(values2)
     logFC = logFC_for_two_means(mean1, mean2, default_value=100)
