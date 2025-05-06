6b08b8cf6cb1c247810b28b6254f0704daa1aa8b
epepelash <epepelash@gmail.com>
2025-05-07 00:33:43 +0300

    vdjdb

--- repseq/io.py
+++ repseq/io.py
@@ -4,6 +4,9 @@ import pandas as pd
 import dill
 import json
 import zipfile
+import requests
+import io
+from .common_functions import extract_segment
 
 def read_yaml_metadata(folder, filename="metadata.yaml"):
     
@@ -294,3 +297,142 @@ def read_mixcr_clonoset(filename):
                                                           'aaSeqFR3': str, 'aaSeqCDR3': str,
                                                           'aaSeqFR4': str, 'refPoints': str})
     return clonoset
+
+
+def vdjdb(vdjdb_dataset=None, drop_method=True, drop_meta=True, drop_cdr3fix=True):
+    """
+    Reads VDJdb datasets and adjusts column names. If `vdjdb_dataset` is `None`, the latest version of the whole database is downloaded.
+    
+    Args:
+        vdjdb_dataset (str): path to a file exported from VDJdb database in .tsv format. 
+            If set to `None` (default), all entries from VDJdb will be downloaded.
+        drop_method (bool): if `True`, `Method` column is excluded
+        drop_meta (bool): if `True`, `Meta` column is excluded
+        drop_cdr3fix (bool): if `True`, `CDR3fix` column is excluded
+
+    Returns:
+        vdjdb_df (pd.DataFrame): DataFrame representation of VDJdb entries
+    """
+    if vdjdb_dataset:
+        vdjdb_df = pd.read_csv(vdjdb_dataset, delimiter='\t')
+        renaming_dict = {'complex.id': 'complex_id',
+                    'Gene': 'chain',
+                    'CDR3': 'cdr3aa',
+                    'V': 'v',
+                    'J': 'j',
+                    'Species': 'species',
+                    'MHC A': 'mhc_a',
+                    'MHC B': 'mhc_b',
+                    'MHC class': 'mhc_class',  
+                    'Epitope': 'epitope',
+                    'Epitope gene': 'epitope_gene',
+                    'Epitope species': 'epitope_species',
+                    'Reference': 'reference',
+                    'Score': 'score',
+                    'Method': 'method',
+                    'Meta': 'meta',
+                    'CDR3fix': 'cdr3fix'}
+    else:
+        renaming_dict = {'complex.id': 'complex_id',
+                    'gene': 'gene',
+                    'cdr3': 'cdr3aa',
+                    'v.segm': 'v',
+                    'j.segm': 'j',
+                    'species': 'species',
+                    'mhc.a': 'mhc_a',
+                    'mhc.b': 'mhc_b',
+                    'mhc.class': 'mhc_class',
+                    'antigen.epitope': 'epitope',
+                    'antigen.gene': 'epitope_gene',
+                    'antigen.species': 'epitope_species',
+                    'reference.id': 'reference_id',
+                    'method': 'method',
+                    'meta': 'meta',
+                    'cdr3fix': 'cdr3fix',
+                    'vdjdb.score': 'score'}
+        api_url = "https://api.github.com/repos/antigenomics/vdjdb-db/releases/latest"
+        response = requests.get(api_url)
+        vdjdb_release = requests.get(response.json()['assets'][0]['browser_download_url']).content
+        with zipfile.ZipFile(io.BytesIO(vdjdb_release)) as zf:
+            filename = [name for name in zf.namelist() if name.endswith('vdjdb.txt')][0]
+            with zf.open(filename) as f:
+                vdjdb_df = pd.read_csv(f, delimiter='\t').drop(columns=[
+                                            'web.method', 
+                                            'web.method.seq', 
+                                            'web.cdr3fix.unmp', 
+                                            'web.cdr3fix.unmp',
+                                            'web.cdr3fix.nc'])
+    vdjdb_df = vdjdb_df.rename(columns=renaming_dict)
+    if not drop_method:
+        method = vdjdb_df.pop('method')
+        method_df = pd.DataFrame.from_dict([json.loads(d) for d in method])
+        vdjdb_df = pd.concat([vdjdb_df, method_df], axis=1)
+        # since entries are added manually, frequency isn't uniform
+        freqs = vdjdb_df['frequency']
+
+        is_fraction = freqs.str.contains(r'^\d+\s*/\s*\d+$', na=False)
+        fractions = freqs[is_fraction].str.extract(r'(\d+)\s*/\s*(\d+)').astype(float)
+        fraction_values = fractions[0] / fractions[1]
+
+        is_percent = freqs.str.endswith('%', na=False)
+        percent_values = freqs[is_percent].str.rstrip('%').astype(float) / 100
+
+        is_number = freqs.str.match(r'^\d*\.?\d+$', na=False)
+        number_values = freqs[is_number].astype(float)
+
+        frequencies = pd.Series(pd.NA, index=freqs.index)
+        frequencies[is_fraction] = fraction_values.values
+        frequencies[is_percent] = percent_values.values
+        frequencies[is_number] = number_values.values
+        vdjdb_df['frequency'] = frequencies
+    else:
+        vdjdb_df.pop('method')
+        
+    if not drop_meta:
+        meta = vdjdb_df.pop('meta')
+        meta_df = pd.DataFrame.from_dict([json.loads(d) for d in meta])
+        meta_df = meta_df.rename(columns=
+                                                                {'study.id': 'study_id',
+                                                                'cell.subset': 'cell_subset',
+                                                                'subject.cohort': 'subject_cohort',
+                                                                'subject.id': 'subject_id',
+                                                                'replica.id': 'replica_id',
+                                                                'clone.id': 'clone_id',
+                                                                'epitope.id': 'epitope_id',
+                                                                'tissue': 'tissue',
+                                                                'donor.MHC': 'donor_mhc',
+                                                                'donor.MHC.method': 'donor_mhc_method',
+                                                                'structure.id': 'structure_id',
+                                                                'samples.found': 'samples_found',
+                                                                'studies.found': 'studies_found'})
+        vdjdb_df = pd.concat([vdjdb_df, meta_df], axis=1)
+    else:
+        vdjdb_df.pop('meta')
+
+    if not drop_cdr3fix:
+        cdr3fix = vdjdb_df.pop('cdr3fix')
+        cdr3fix_df = pd.DataFrame.from_dict([json.loads(d) for d in cdr3fix])
+        cdr3fix_df = cdr3fix_df.rename(columns={'cdr3': 'cdr3aa_new',
+                                                                         'cdr3_old': 'cdr3aa_old'})
+        vdjdb_df = pd.concat([vdjdb_df, cdr3fix_df], axis=1)
+    else:
+        vdjdb_df.pop('cdr3fix')
+    order = ['complex_id', 'chain', 'cdr3aa', 'v', 'j', 'species', 'mhc_a', 'mhc_b',
+       'mhc_class', 'epitope', 'epitope_gene', 'epitope_species', 'reference',
+       'score', 'identification', 'frequency', 'singlecell', 'sequencing',
+       'verification', 'study_id', 'cell_subset', 'subject_cohort',
+       'subject_id', 'replica_id', 'clone_id', 'epitope_id', 'tissue',
+       'donor_mhc', 'donor_mhc_method', 'structure_id', 'samples_found',
+       'studies_found', 'cdr3aa_new', 'cdr3aa_old', 'fixNeeded', 'good',
+       'jCanonical', 'jFixType', 'jId', 'jStart', 'vCanonical', 'vEnd',
+       'vFixType', 'vId']
+    existing_columns = [col for col in order if col in vdjdb_df.columns]
+    vdjdb_df['v'] = vdjdb_df['v'].apply(lambda x: extract_segment(x))
+    vdjdb_df['j'] = vdjdb_df['j'].apply(lambda x: extract_segment(x))
+    def to_numeric(s):
+        try:
+            return pd.to_numeric(s)
+        except ValueError:
+            return s
+    vdjdb_df = vdjdb_df.apply(to_numeric)
+    return vdjdb_df[existing_columns]
