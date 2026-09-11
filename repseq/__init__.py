82a17bb2860fc4fa31ba827ea4fce6c549edc885
Mikhail Myshkin <mikhailmyshkin.ibch@gmail.com>
2026-09-11 08:54:16 +0300

    version info update

--- repseq/__init__.py
+++ repseq/__init__.py
@@ -1,3 +1,5 @@
 from .clonoset import Clonoset, standardize_to_vdjtools_columns
+from importlib.metadata import version
 
 __all__ = ["Clonoset", "standardize_to_vdjtools_columns"]
+__version__ = version("repseq")
